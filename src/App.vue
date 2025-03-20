3f0e06b41e02ff4b2ebd267d384af50fd88e2654
shihfuwang <fuwang40@gmail.com>
2025-03-20 23:24:30 +0800

    環境建立vue + tailwind + scss)

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="title">
+    <h1 class="text-3xl font-bold underline text">
+      Hello world!
+    </h1>
+  </div>
+
+  <h1>
+    Hello world!
+  </h1>
+</template>
+
+<style scoped lang="scss">
+.title{
+  max-width:size(1020);
+  max-height: size(170);
+  border-radius: size(15);
+  border-color: aqua;
+  border-width: 2px;
+}
+.text{
+
+  font-size: size(12);
+}
+</style>
