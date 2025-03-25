dc0689347d86061ee66551bdf88e25dbeebd5b98
shihfuwang <fuwang40@gmail.com>
2025-03-25 10:36:01 +0800

    fix a<href>

--- src/sessions/sideProject/components/TextBlock.vue
+++ src/sessions/sideProject/components/TextBlock.vue
@@ -7,18 +7,22 @@ const props = defineProps<{
 }>()
 
 function handleClick(link: { name: string; url: string }, event: MouseEvent) {
-  if (link.url.includes('localhost:3000')) {
-    event.preventDefault()
+  const isLocalTrigger = link.url.includes('localhost')
 
+  event.preventDefault()
+
+  if (isLocalTrigger) {
     fetch(link.url)
       .then(res => res.text())
       .then(msg => {
-        alert(msg) // 顯示：✅ lottery.bat 已啟動！
+        alert(msg)
       })
       .catch(err => {
-        alert('❌ 啟動失敗，請確認本地 server 是否開啟')
+        alert('❌ 無法啟動本地程式，請確認 server 是否已開啟')
         console.error(err)
       })
+  } else {
+    window.open(link.url, '_blank')
   }
 }
 </script>
@@ -27,15 +31,13 @@ function handleClick(link: { name: string; url: string }, event: MouseEvent) {
   <div>
     <Title :title="props.title" :size="40" />
     <ul class="text-green-600 space-y-1 text-[16px] md:text-[20px]">
-      <li v-for="(link, index) in props.links" :key="index">
-        <a
-          :href="link.url"
-          target="_blank"
-          class="hover:underline"
-          @click="(e) => handleClick(link, e)"
-        >
-          {{ link.name }}
-        </a>
+      <li
+        v-for="(link, index) in props.links"
+        :key="index"
+        class="cursor-pointer hover:underline"
+        @click="(e) => handleClick(link, e)"
+      >
+        {{ link.name }}
       </li>
     </ul>
   </div>
